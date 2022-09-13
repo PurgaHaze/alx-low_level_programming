12afd5ea72a96102d14e70d167b5e7e5911f2b0b
Kgaugelo <PurgatoryHaze@gmail.com>
2022-09-12 18:57:38 -0700

    Write

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 /**
  * main - prints a string
- * Return: 1 (Success)
+ *
+ * Return: 0 (Success)
  */
 int main(void)
 {
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 61);
+
 	return (1);
 }
