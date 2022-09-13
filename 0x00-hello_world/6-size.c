a15e3a4b4a74689a5c0721fda2d06ba1909acf1e
Kgaugelo <PurgatoryHaze@gmail.com>
2022-09-12 17:54:13 -0700

    Size of different types

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - getting the size of various types
+ *
+ * Return: Always 0 (Success)
+ *
+ */
+int main(void)
+{
+	char x;
+	int a;
+	long int b;
+	long long int c;
+	float d;
+
+
+	printf("Size of char:%lu byte(s)\n", (unsigned long)sizeof(x));
+	printf("Size of int:%lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("Size of long int:%lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("Size of long long int:%lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("Size of float:%lu byte(s)\n", (unsigned long)sizeof(f));
+	return (0);
+}
