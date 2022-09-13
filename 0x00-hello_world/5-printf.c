41ec730e650b55d702796395b5b9a34f03052590
Kgaugelo <PurgatoryHaze@gmail.com>
2022-09-12 17:13:21 -0700

    Printing using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*
+ * Print_F-printing usinf printf
+ * void: the string itself
+ * Return: Always 0 (Success)
+ */
+int Print_F(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return(0);
+}
