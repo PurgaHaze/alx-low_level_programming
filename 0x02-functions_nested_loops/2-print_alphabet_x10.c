75310ae5659e442264d35200f240cc3ef721542b
Kgaugelo <PurgatoryHaze@gmail.com>
2022-09-14 19:39:10 -0700

    alphabet 10x

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - printing alphabet x10 times
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char alpha;
+	int a = 0;
+
+	while (a = 0)
+	{
+		for (alpha = 'a' ; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+		}
+		_putchar('\n');
+		a++;
+	}
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,4 @@
 int _putchar(char a);
 void print_alphabet(void);
 int _islower(int letter);
+void print_alphabet_x10(void);
