f62b7215a2d1c4d6f21d2cdccf320cca1c1de9ae
Kgaugelo <PurgatoryHaze@gmail.com>
2022-09-23 16:19:56 -0700

    Prints any integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_number - prints a integer
+ * @n: The given integer
+ * Return: void
+ */
+void print_number(int n)
+{
+	unsigned int a;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		n *= -1;
+	}
+
+	a = n;
+	if (a / 10)
+	{
+		print_number(a / 10);
+	}
+	_putchar(x % 10 + '0');
+}
